ccad9c09775066a2544974811b2803c6ca7bfa9a
Li, Shaopeng Bruce <Bli@webmd.net>
2017-08-10 16:44:14 -0400

    import request model and implement function made get request to remote

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpModule } from '@angular/http';
 import { FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 
@@ -7,18 +8,21 @@ import { AppComponent } from './app.component';
 import { HelloComponent } from './hello/hello.component';
 import { InputFormComponent } from './input-form/input-form.component';
 import { SkillComponent } from './skill/skill.component';
+import { ReqestComponent } from './reqest/reqest.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HelloComponent,
     InputFormComponent,
-    SkillComponent
+    SkillComponent,
+    ReqestComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    HttpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
