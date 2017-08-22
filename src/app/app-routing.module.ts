9cc0a7dbd67bd16c847c978e1500660434c1ac6f
Li, Shaopeng Bruce <Bli@webmd.net>
2017-08-22 13:17:22 -0400

    create app-routing-module and put routing logic inside that module

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,28 @@
+import { NgModule }              from '@angular/core';
+import { RouterModule, Routes }  from '@angular/router';
+
+import { HelloComponent } from './hello/hello.component';
+import { SkillListComponent } from './skill-list/skill-list.component';
+import { ReqestComponent } from './reqest/reqest.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+
+const appRoutes: Routes = [
+  {path: 'landing', component: HelloComponent},
+  {path: 'skillList', component: SkillListComponent},
+  {path: 'searching', component: ReqestComponent},
+  {path: '', redirectTo:'/landing', pathMatch: 'full'},
+  {path: '**', component: PageNotFoundComponent}
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    )
+  ],
+  exports: [
+    RouterModule
+  ]
+})
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpModule } from '@angular/http';
 import { FormsModule, ReactiveFormsModule} from '@angular/forms';
-import { RouterModule, Routes } from '@angular/router';
+import {AppRoutingModule} from './app-routing.module';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello/hello.component';
@@ -10,19 +10,13 @@ import { InputFormComponent } from './input-form/input-form.component';
 import { SkillComponent } from './skill/skill.component';
 import { ReqestComponent } from './reqest/reqest.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { SkillListComponent } from './skill-list/skill-list.component';
 import { HighlightDirectiveDirective } from './highLightDirective/highlight-directive.directive';
 import { ShowHideDirectiveDirective } from './showHideDrective/show-hide-directive.directive';
 import { FormatPipePipe } from './formatPipe/format-pipe.pipe';
-import { SkillListComponent } from './skill-list/skill-list.component';
 import { SkillList } from './share/skillList.service';
 
-const appRoutes: Routes = [
-  {path: 'landing', component: HelloComponent},
-  {path: 'skillList', component: SkillListComponent},
-  {path: 'searching', component: ReqestComponent},
-  {path: '', redirectTo:'/landing', pathMatch: 'full'},
-  {path: '**', component: PageNotFoundComponent}
-];
+
 
 @NgModule({
   declarations: [
@@ -42,9 +36,7 @@ const appRoutes: Routes = [
     FormsModule,
     ReactiveFormsModule,
     HttpModule,
-    RouterModule.forRoot(
-      appRoutes
-    )
+    AppRoutingModule
   ],
   providers: [SkillList],
   bootstrap: [AppComponent]
